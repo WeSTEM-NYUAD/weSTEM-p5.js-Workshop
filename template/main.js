7944bb7f39cbf2a404c4ac11de6fa5644f206971
Brooke Hopkins <brooke.hopkins@nyu.edu>
2016-11-19 22:56:35 +0400

    worshop basic examples

---
+++ template/main.js
@@ -0,0 +1,12 @@
+function drawCanvas(){
+	//Get the canvas
+	var canvas = document.getElementById('myCanvas');
+	//Set the context
+	var ctx = canvas.getContext('2d');
+
+	//your code and shapes go here
+}
+
+$('document').ready(function(){
+	drawCanvas();
+});
