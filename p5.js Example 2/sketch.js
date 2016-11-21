8f5d2bda44e55886b14c34f09487bb88b39fd3a5
ydc223 <ydc223@nyu.edu>
2016-11-21 18:12:41 +0400

    Example 2 added

--- p5.js Example 2/sketch.js
+++ p5.js Example 2/sketch.js
@@ -5,21 +5,28 @@ function setup() {
     stroke(0);
 
     /* Face */
-    fill(147,112,219); // purple
-    ellipse(600,300,300,300);
+    fill(84,255,159); // light green
+    ellipse(600,300,350,300);
 
     /* Eyes */
-    fill(50,230,180); //blueish-green
+    fill(227,168,105); //light brown
     ellipse(550,240,50,50);
     ellipse(650,240,50,50);
     fill(0);
     ellipse(553,245,10,10);
     ellipse(653,245,10,10);
 
+
     /* Mouse */
-    noFill();
     strokeWeight(2);
-    arc(600, 350, 150, 90, 0, PI);
+    line(500,350,700,350);
+    fill(255);
+
+    /* Teeth */
+    rect(590,350,10,20);
+    rect(610,350,10,20);
+    line(495,345,505,355);
+    line(695,355,705,345);
 
 }
 
